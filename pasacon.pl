e44d7f36a41c7d82d5e09367013d8bfa4a48eac1
Mike <mike@afk47.org>
2021-11-20 23:12:39 +0800

    minor formatting

--- pasacon.pl
+++ pasacon.pl
@@ -19,19 +19,19 @@ my @deadarray;
 my $FH0;
 my $fLineCnt = "0";
 my $p;
-my $user = "mike";
+my $user    = "mike";
 my $command = "uptime|sed \'s/.*up \\([^,]*\\), .*/\\1/\'|tr -d '\\n' && yum -q check-update >/dev/null 2>&1";
-my $file = "/mnt/c/Users/mike/OneDrive/Desktop/test.html";
-my $date = strftime "%F %H:%M", localtime;
-my $quiet = "0";
+my $file    = "/mnt/c/Users/mike/OneDrive/Desktop/test.html";
+my $date    = strftime "%F %H:%M", localtime;
+my $quiet   = "0";
 
 ## Main ##
 
 if ( $quiet == "1" ) {
-open (my $STDOLD, '>&', STDOUT);
-open (my $STERR, '>&', STDERR);
-open (STDERR, '>', '/dev/null');
-open (STDERR, '>', '/dev/null');
+    open( my $STDOLD, '>&', STDOUT );
+    open( my $STERR,  '>&', STDERR );
+    open( STDERR,     '>',  '/dev/null' );
+    open( STDERR,     '>',  '/dev/null' );
 }
 
 &PingTest();
@@ -66,8 +66,7 @@ sub GetData {
     push( @array1, $_ );
     push( @array2, $out );
     if ( $exit == '100' ) {
-        push( @array3, "<td style=\"background-color: #ff9900\">Updates Pending</td>"
-        );
+        push( @array3, "<td style=\"background-color: #ff9900\">Updates Pending</td>" );
 
         # print "Debugging: Got code $exit should be 100\n";
     }
@@ -79,7 +78,7 @@ sub GetData {
     else {
         push( @array3, "<td style=\"background-color: #ff0000\">ERROR!</td>" );
 
-      #print "Debugging: Got code $exit should be 1 or anything but 100 or 0\n";
+        #print "Debugging: Got code $exit should be 1 or anything but 100 or 0\n";
     }
 
     @merged = ( \@array1, \@array2, \@array3 );
@@ -120,18 +119,14 @@ sub PrintData {
 EOF
     foreach my $i ( 0 .. $#{ $merged[0] } ) {
         print FH (
-            "<tr><td>",  $merged[0][$i], "</td><td>", $merged[1][$i],
-             $merged[2][$i], "</tr>\n"
-        
-    );
+            "<tr><td>",     $merged[0][$i], "</td><td>", $merged[1][$i],
+            $merged[2][$i], "</tr>\n"
+
+        );
     }
     if (@deadarray) {
         foreach (@deadarray) {
-            print FH (
-            "<tr><td>", $_, 
-            "</td><td colspan=2, style=\"background-color: #ff0000\">",
-            "OFFLINE", "</td></tr>\n"
-            );
+            print FH ( "<tr><td>", $_, "</td><td colspan=2, style=\"background-color: #ff0000\">", "OFFLINE", "</td></tr>\n" );
         }
     }
 
